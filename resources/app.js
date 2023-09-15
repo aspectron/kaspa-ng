894ae0f3f65d482fadf9358fbe1d97cd4c149daf
Surinder Singh Matoo <surinder83singh@gmail.com>
2023-09-15 06:33:06 +0530

    wip

---
+++ resources/app.js
@@ -0,0 +1,9 @@
+// We disable caching during development so that we always view the latest version.
+// if ('serviceWorker' in navigator && window.location.hash !== "#dev") {
+//     window.addEventListener('load', function () {
+//         navigator.serviceWorker.register('./pwa.js');
+//     });
+// }
+
+import init from '/kaspa-egui-57c7a8dd13e092be.js';
+init('/kaspa-egui-57c7a8dd13e092be_bg.wasm');
