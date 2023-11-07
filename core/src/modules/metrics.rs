33dbb803988daecbaca7be48651a0cda5a860f28
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-11-07 03:48:33 +0200

    wip - metrics

--- core/src/modules/metrics.rs
+++ core/src/modules/metrics.rs
@@ -14,6 +14,11 @@ impl Metrics {
 }
 
 impl ModuleT for Metrics {
+
+    fn style(&self) -> ModuleStyle {
+        ModuleStyle::Default
+    }
+
     fn render(
         &mut self,
         wallet: &mut Wallet,
@@ -21,33 +26,12 @@ impl ModuleT for Metrics {
         _frame: &mut eframe::Frame,
         ui: &mut egui::Ui,
     ) {
-        // wallet.style()
 
-        ui.style_mut().text_styles = wallet.default_style.text_styles.clone();
-
-        // ui.heading("Settings");
-        // ui.separator();
-        // ui.label("This is the settings page");
-
-        // - wRPC JSON, wRPC BORSH, gRPC
-        // - PUBLIC RPC LISTEN !
-        // - MAINNET, TESTNET-10, TESTNET-11
-        // - CUSTOM RPC PORTS
-        // - EXTERNAL IP
-        // -
-        // ---
-        // - pub connect_peers: Vec<ContextualNetAddress>,
-        // - pub add_peers: Vec<ContextualNetAddress>,
-        // - pub outbound_target: usize,
-        // - pub inbound_limit: usize,
-        // - pub rpc_max_clients: usize, gRPC
-        // - pub enable_unsynced_mining: bool,
-        // - pub enable_mainnet_mining: bool,
-        // - pub perf_metrics: bool,
-        // - pub perf_metrics_interval_sec: u64,
+        ui.heading("Metrics");
+        ui.separator();
 
         CollapsingHeader::new("Kaspa Node")
-            .default_open(false)
+            .default_open(true)
             .show(ui, |ui| {
                 // ui.label("This is the settings page");
 
