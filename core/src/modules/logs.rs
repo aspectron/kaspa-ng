7a036e02b916fee5d58d55f7537cf4de349809ec
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-11-28 08:49:44 +0200

    copy logs to clipboard button

--- core/src/modules/logs.rs
+++ core/src/modules/logs.rs
@@ -24,21 +24,36 @@ impl ModuleT for Logs {
         _core: &mut Core,
         _ctx: &egui::Context,
         _frame: &mut eframe::Frame,
-        _ui: &mut egui::Ui,
+        ui: &mut egui::Ui,
     ) {
+        use egui_phosphor::light::CLIPBOARD_TEXT;
 
         #[cfg(not(target_arch = "wasm32"))]
         egui::ScrollArea::vertical()
             .id_source("node_logs")
             .auto_shrink([false; 2])
             .stick_to_bottom(true)
-            .show(_ui, |ui| {
+            .show(ui, |ui| {
 
                 for log in self.runtime.kaspa_service().logs().iter() {
                     ui.label(RichText::from(log));
                 }
             });
 
+        let copy_to_clipboard = Button::new(format!(" {CLIPBOARD_TEXT} "));
 
+        let screen_rect = ui.ctx().screen_rect();
+        let button_rect = Rect::from_min_size(
+            pos2(screen_rect.max.x - 48.0, screen_rect.min.y + 32.0),
+            vec2(38.0, 20.0),
+        );
+
+        if ui.put(button_rect, copy_to_clipboard)
+            .on_hover_text_at_pointer(i18n("Copy logs to clipboard"))
+            .clicked() {
+                let logs = self.runtime.kaspa_service().logs().iter().map(|log| log.to_string()).collect::<Vec<String>>().join("\n");
+                ui.output_mut(|o| o.copied_text = logs);
+                runtime().notify(UserNotification::info(i18n("Copied to clipboard")).short())
+            }
     }
 }
