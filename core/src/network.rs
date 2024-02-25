af3634b12357a6a403716712ff046adeff0b8814
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-02-25 19:04:11 +0200

    fix typo

--- core/src/network.rs
+++ core/src/network.rs
@@ -21,7 +21,7 @@ impl std::fmt::Display for Network {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Network::Mainnet => write!(f, "mainnet" ),
-            Network::Testnet10 => write!(f, "tesntet-10"),
+            Network::Testnet10 => write!(f, "testnet-10"),
             Network::Testnet11 => write!(f, "testnet-11"),
         }
     }
