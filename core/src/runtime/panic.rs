3544dcf39f6266488d8c4b680da798a83439a610
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-05-16 21:40:38 +0300

    create kaspa-ng.log in case a panic occurs

--- core/src/runtime/panic.rs
+++ core/src/runtime/panic.rs
@@ -1,8 +1,11 @@
+use std::fs;
 use std::panic;
 
 pub fn init_graceful_panic_handler() {
     let default_hook = panic::take_hook();
     panic::set_hook(Box::new(move |panic_info| {
+        let _ = fs::write("kaspa-ng.log", format!("{:#?}", panic_info));
+
         default_hook(panic_info);
         crate::runtime::abort();
     }));
@@ -11,6 +14,8 @@ pub fn init_graceful_panic_handler() {
 pub fn init_ungraceful_panic_handler() {
     let default_hook = panic::take_hook();
     panic::set_hook(Box::new(move |panic_info| {
+        let _ = fs::write("kaspa-ng.log", format!("{:#?}", panic_info));
+
         default_hook(panic_info);
         println!("Exiting...");
         std::process::exit(1);
